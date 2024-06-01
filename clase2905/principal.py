2d69199412a444923af9b267f9555b548ac62b67
Pablo Scrigna <pablo.s@hyperbloq.com>
2024-06-01 00:22:55 -0300

    material de las clases del 29/05 y 30/05

---
+++ clase2905/principal.py
@@ -0,0 +1,9 @@
+import secundario
+
+
+name = __name__
+
+print("programa principal")
+print(f"principal name: {name}")
+print(secundario.suma(5,5))
+print(secundario.CURSO)
---
+++ clase2905/secundario.py
@@ -0,0 +1,15 @@
+
+CURSO = "PYTHON AVANZADO"
+
+def suma(a, b):
+    return a + b
+
+
+if __name__ == "__main__":
+    print("me estan invocando a mi directamente")
+    print(CURSO)
+    print(suma(5,7))
+
+
+
+    
